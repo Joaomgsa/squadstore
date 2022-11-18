b55d1afc64d0e15c7f0119421e895e0074f4085c
Joaomgsa <joaomgsa@gmail.com>
2022-11-17 23:31:19 -0300

    string de conexao ambiente de produção

--- src/main/java/br/com/squadstore/SquadstoreApplication.java
+++ src/main/java/br/com/squadstore/SquadstoreApplication.java
@@ -2,6 +2,8 @@ package br.com.squadstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
 public class SquadstoreApplication {
